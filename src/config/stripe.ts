7443b7f4d25a0ebb48cb765458dae6b26540a3cf
Ross Cohen <rosscohen1990@gmail.com>
2023-10-09 18:59:36 -0400

    Update stripe.ts

--- src/config/stripe.ts
+++ src/config/stripe.ts
@@ -2,8 +2,8 @@ export const PLANS = [
   {
     name: 'Free',
     slug: 'free',
-    quota: 10,
-    pagesPerPdf: 5,
+    quota: 100,
+    pagesPerPdf: 15,
     price: {
       amount: 0,
       priceIds: {
@@ -15,8 +15,8 @@ export const PLANS = [
   {
     name: 'Pro Plan',
     slug: 'pro',
-    quota: 50,
-    pagesPerPdf: 25,
+    quota: 500,
+    pagesPerPdf: 55,
     price: {
       amount: 14,
       priceIds: {
